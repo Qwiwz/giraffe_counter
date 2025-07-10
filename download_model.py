af629a08e28d0df201263bafcd2cad5424da60cb
Qwiwz <Liguratic@yandex.ru>
2025-07-10 16:21:50 +0300

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
 
-# Модель автоматически скачается при первом запуске
 model = YOLO('yolov8n.pt')
-model.export(format='torchscript')  # Конвертация для оптимизации
+model.export(format='torchscript')
